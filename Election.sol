c7273cf45f17d8d4d737e3bc99ac62e969c14592
Vinicius <vinicius.macelai@gmail.com>
2019-09-04 15:34:13 -0300

    first election contract

---
+++ Election.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.11;
+
+contract HeliosElection {
+    
+    string uuid;
+    string name;
+    string cast_url;
+    uint256 created_at;
+    uint256 voting_starts;
+    uint256 voting_ends;
+    
+    struct Public_Key {
+        string p;
+        string q;
+        string g;
+        string y;
+    }
+    
+    struct Private_Key {
+        string p;
+    }
+    
+    Private_Key private_key;
+    Public_Key public_key;
+    
+    struct Question {
+        string question;
+        string[] anwsers;
+    }
+    
+    Question[] questions;
+    
+    
+    constructor() public {
+
+    }
+    
+}
